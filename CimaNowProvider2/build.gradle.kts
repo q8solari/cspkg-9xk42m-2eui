f8480d85c2bf5146d5c6147603081841fb32f5c3
q8solari <q8solari@gmail.com>
2026-07-06 02:11:07 +0300

    Create build.gradle.kts

---
+++ CimaNowProvider2/build.gradle.kts
@@ -0,0 +1,12 @@
+version = 1
+
+cloudstream {
+    authors = listOf("private")
+    status = 3
+    tvTypes = listOf("Movie", "TvSeries")
+    language = "ar"
+}
+
+android {
+    namespace = "com.cimanow"
+}
